dffa409d2a3088fc3e76cdee92edc8d791b0b44e
NinaMil <011@gmail.com>
2023-10-15 11:08:27 +0200

    Refactor

--- example.py
+++ example.py
@@ -1,11 +1,8 @@
-def sweap_max(items: list) -> list:
-    max_pos = 0
-    for i in range(len(items)):
-        if items[i] >= items[max_pos]:
-            max_pos = i
+def sweep_max(items: list) -> list:
+    max_pos = items.index(max(items))
     items[0], items[max_pos] = items[max_pos], items[0]
     return items
 
 
 if __name__ == "__main__":
-    print(sweap_max([10, 9, 4, 32, 4]))
+    print(sweep_max([10, 9, 4, 32, 4]))
