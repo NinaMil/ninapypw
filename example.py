7d3bc22c06b4f43af168e39a366f84c885c34ec2
NinaMil <011@gmail.com>
2023-10-15 11:25:09 +0200

    add tuple example

--- example.py
+++ example.py
@@ -1,8 +1,13 @@
-def sweep_max(items: list) -> list:
+"""def sweep_max(items: list) -> list:
     max_pos = items.index(max(items))
     items[0], items[max_pos] = items[max_pos], items[0]
     return items
 
 
 if __name__ == "__main__":
-    print(sweep_max([10, 9, 4, 32, 4]))
+    print(sweep_max([10, 9, 4, 32, 4]))"""
+
+items=(1, 2, 3, 4, 5)
+
+for item in items:
+    print (item)
